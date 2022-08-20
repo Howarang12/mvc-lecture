250914b4390e0cc46aaa2515dffbad80ef3f595d
Howard Wang <howard122696@gmail.com>
2022-08-19 18:01:33 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/Blog')
+
+const DB_STRING = 'mongodb+srv://howarang:hw122696@cluster0.q1cspqo.mongodb.net/mvc?retryWrites=true&w=majority'
+mongoose.connect(DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => {
+    console.log('connected to db...')
+    app.listen(3000, () => console.log('server running... '))
+  })
+  .catch((err) => console.log(err))
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(morgan('dev'))
+
+
+app.get('/', (req, res) => {
+  res.redirect('/blogs')
+})
+
+app.get('/blogs', async (req, res) => {
+  let blogs = await Blog.find().sort({createdAt: -1})
+  res.render('index', {title: 'All blogs', blogs})
+
+})
+
+app.get('/model', (req, res) => {
+  res.render('model', {title: 'Model'})
+})
+
+app.get('/view', (req, res) => {
+  res.render('view', {title: 'View'})
+})
+
+app.get('/controller', (req, res) => {
+  res.render('controller', {title: 'Controller'})
+})
+
+app.use((req, res) => {
+  res.status(404).render('404', {title: '404'})
+})
